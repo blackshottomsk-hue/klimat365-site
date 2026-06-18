e15e603e4a8ef4f6d5f761772afd08798274c3fb
Роман Подковыров <webmeneg02@icaall.local>
2026-06-18 10:30:33 +0300

    Add Yandex Metrika counter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import type { Metadata, Viewport } from "next";
+import Script from "next/script";
 import Footer from "../components/layout/Footer";
 import Header from "../components/layout/Header";
 import ModalRoots from "../components/layout/ModalRoots";
@@ -79,6 +80,20 @@ export default function RootLayout({
         <StickyCTA />
         <ModalRoots />
         <CookieBanner />
+        <Script id="yandex-metrika" strategy="afterInteractive">{`
+          (function(m,e,t,r,i,k,a){
+            m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+          })(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=109953795','ym');
+          ym(109953795,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:"dataLayer",referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});
+        `}</Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/109953795" style={{position:"absolute",left:"-9999px"}} alt="" />
+          </div>
+        </noscript>
       </body>
     </html>
   );
